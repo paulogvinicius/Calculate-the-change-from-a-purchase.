29a31ba79e8bac7b430a794bc9f7c8d1fe3811f8
Paulo Vinícius <129632438+paulogvinicius@users.noreply.github.com>
2026-06-06 21:16:28 -0400

    Refactor main.java to include CalcularTroco class

--- main.java
+++ main.java
@@ -1,6 +1,11 @@
-double compra = 35.50;
-double pago = 50.00;
+public class CalcularTroco {
+    public static void main(String[] args) {
+      double compra = 35.50;
+      double pago = 50.00;
 
-double troco = pago - compra;
+      double troco = pago - compra;
 
-System.out.println("Troco: R$ " + troco);
+      System.out.println("Troco: R$ " + troco);
+
+    }
+}
